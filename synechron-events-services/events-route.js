45c54f8d5b0af39d316dbf475123fd78decd240d
spg05 <41828403+spg05@users.noreply.github.com>
2020-01-22 15:55:05 +0530

    Update events-route.js

--- synechron-events-services/events-route.js
+++ synechron-events-services/events-route.js
@@ -1,3 +1,7 @@
+
+https://sso.capgemini.com/opensso/XUI/?realm=/Capgemini&forward=true&spEntityID=f4b13b73-7725-4154-99ea-cb9d6a4855e0&goto=/SSORedirect/metaAlias/Capgemini/idpassignmentPro?ReqID%3DI0_rhtUY7L1yncaqDlAR2GNTTrtTOSeDB2LOIgAiirdoR_tanS1Snltpf8Li-F%26index%3Dnull%26acsURL%3D%26spEntityID%3Df4b13b73-7725-4154-99ea-cb9d6a4855e0%26binding%3D&AMAuthCookie=#login/
+
+
 const express = require('express');
 const eventRoutes = express.Router();
 var eventObj = require('./web-api/event-api');
